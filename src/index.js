82bd2edc6619221944575f620aac117dd3c5165f
Patrick Ly <112744444447551+patrickly1@users.noreply.github.com>
2024-04-18 13:57:51 -0400

    Added functions to extract weather data from the API JSON, and updated the DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import _ from "lodash";
 import "./style.css";
-import { getCurrentWeather, getForecast } from "./weather-data";
+import { getCurrentWeather, getForecast} from "./weather-data";
 
 getCurrentWeather();
 getForecast();
+
+function getLocationData() {
+    return getCurrentWeather().then(data => data ? data.location : null);
+}
+
+function getCurrentWeatherData() {
+    return getCurrentWeather().then(data => data ? data.current : null);
+}
+
+
+function updateDOMWithLocation() {
+    getLocationData().then(location => {
+        if (location) {
+            document.getElementById('locationName').textContent = `Location Name: ${location.name}`;
+            document.getElementById('locationRegion').textContent = `Location Region: ${location.region}`;
+            document.getElementById('locationCountry').textContent = `Location Country: ${location.country}`;
+        } else {
+            console.log("error");
+        };
+    });
+};
+
+function updateDOMWithCurrentWeather() {
+    getCurrentWeatherData().then(current_weather => {
+        if (current_weather) {
+            document.getElementById('feelslike').textContent = `Feels like: ${current_weather.feelslike_c}°C`;
+        }
+        
+    });
+}
+
+
+updateDOMWithLocation();
+updateDOMWithCurrentWeather();
--- src/weather-data.js
+++ src/weather-data.js
@@ -3,28 +3,46 @@ const weather = document.querySelector('.container');
 let location = "vancouver";
 let currentWeatherURL = 'https://api.weatherapi.com/v1/current.json?key=9d73c117f8704d6c91f235638241604&q=' + location;
 let forecastURL = 'https://api.weatherapi.com/v1/forecast.json?key=9d73c117f8704d6c91f235638241604&q=' + location + '&days=3'
+let cachedCurrentWeatherData = null;
+let cachedForecastData = null;
 
 async function getCurrentWeather() {
+  if (cachedCurrentWeatherData !== null) return cachedCurrentWeatherData;
   try {
     const response = await fetch(currentWeatherURL, {mode: 'cors'});
     const weatherData = await response.json();
     console.log(weatherData);
+    cachedCurrentWeatherData = weatherData;
+    return weatherData
   } catch (error) {
     console.log('Failed to fetch weather data:', error);
   };
 };
 
 async function getForecast() {
+  if (cachedForecastData !== null) return cachedForecastData; 
   try {
     const response = await fetch(forecastURL, {mode: 'cors'});
     const weatherData = await response.json();
+    cachedForecastData = weatherData;
     console.log(weatherData);
   } catch (error) {
     console.log('Failed to fetch weather data:', error);
   };
 };
 
+//async function getLocationData() {
+//    try {
+//        const data = await getCurrentWeather();
+//        console.log("Forecast location", data.location.name);
+//    } catch (error) {
+//        console.error("Error handling in calling function", error);
+//    };
+//   };
+
 export {
     getCurrentWeather,
-    getForecast,
+    getForecast
 }
+ //   getLocationData
+//}
